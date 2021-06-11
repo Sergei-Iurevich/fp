7c12124820e380b9e6a65cb854217c70a1f125b8
Sergei-Iurevich <74328492+Sergei-Iurevich@users.noreply.github.com>
2021-06-11 09:11:12 +0000

    Deploying to gh-pages from  @ d947cd70729a3a1573da0f4194f2a705b3c8d65f 🚀

---
+++ index_bundle.js
@@ -0,0 +1,113 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/ts/index.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/ts/Parallel.ts":
+/*!****************************!*\
+  !*** ./src/ts/Parallel.ts ***!
+  \****************************/
+/*! exports provided: Parallel */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Parallel\", function() { return Parallel; });\nclass Parallel {\n  constructor(numberOfThreads) {\n    this.numberOfThreads = numberOfThreads;\n  }\n\n  async jobs(...jobsArgs) {\n    const processArray = [...jobsArgs];\n    const resultArray = [];\n    const processStack = [];\n    const history = new Set();\n\n    const recurFunc = () => {\n      if (processArray.length > 0) {\n        while (processStack.length < this.numberOfThreads) {\n          const process = processArray.shift();\n\n          if (process) {\n            processStack.push(process());\n          }\n        }\n\n        processStack.forEach((item, indexItem) => {\n          if (!history.has(item)) {\n            history.add(item);\n            item.then(result => {\n              resultArray.push(result);\n              processStack.splice(indexItem, 1);\n              recurFunc();\n            });\n          }\n        });\n      }\n\n      return resultArray;\n    };\n\n    return recurFunc();\n  }\n\n}\n\n//# sourceURL=webpack:///./src/ts/Parallel.ts?");
+
+/***/ }),
+
+/***/ "./src/ts/index.ts":
+/*!*************************!*\
+  !*** ./src/ts/index.ts ***!
+  \*************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Parallel__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Parallel */ \"./src/ts/Parallel.ts\");\n\n\n(async function xxx() {\n  const runner = new _Parallel__WEBPACK_IMPORTED_MODULE_0__[\"Parallel\"](3);\n  console.log(await runner.jobs(() => new Promise(resolve => setTimeout(resolve, 10, 1)), () => new Promise(resolve => setTimeout(resolve, 50, 2)), () => new Promise(resolve => setTimeout(resolve, 20, 3)), () => new Promise(resolve => setTimeout(resolve, 90, 4)), () => new Promise(resolve => setTimeout(resolve, 30, 5))));\n})();\n\n//# sourceURL=webpack:///./src/ts/index.ts?");
+
+/***/ })
+
+/******/ });
