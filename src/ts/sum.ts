b65f5d71a0862b9a83e96d5d03f1d721e31f1556
Sergei Iurevich <lask2su@gmail.com>
2021-06-11 11:15:05 +0300

    Add function sum

---
+++ src/ts/sum.ts
@@ -0,0 +1,23 @@
+export function sum(...args: number[]): any {
+    if (args.length === 0) {
+        return 0;
+    }
+    const arg = args[0];
+    let result = arg;
+    let t = 0;
+
+    function recurFunc(b: number) {
+        if (t > 0) {
+            result = arg;
+            t = 0;
+        }
+        result += b;
+        return recurFunc;
+    }
+
+    recurFunc.toString = function () {
+        t += 1;
+        return result;
+    }
+    return recurFunc;
+}
